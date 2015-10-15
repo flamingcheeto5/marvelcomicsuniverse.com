2118f7e014713a5d607b32336627c2f1ba255104
David Banks <david.banks0889@gmail.com>
2015-10-15 08:49:18 -0500

    Merge master/master into HEAD
    Conflicts:
    	public_html/css/main.css
    	public_html/index.html

--- public_html/public_html/model/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION=array();
-setcookie(session_name(),"",time()-3600);
-session_destroy();
-header("Location: login.php?id=<div class='h2'>You are successfully logged out!<div>");
-?>
