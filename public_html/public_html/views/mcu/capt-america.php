1843cb6bf15e199b6cd2f32aa801468f83c1adc7
David Banks <david.banks0889@gmail.com>
2015-10-15 14:19:05 -0500

    Merge origin/origin
    Conflicts:

---
+++ public_html/public_html/views/mcu/capt-america.php
@@ -0,0 +1,18 @@
+<?php
+include ('../header.php'); 
+
+?>
+
+</br>
+
+<div id="movie-trailer">
+<iframe width="560" height="315" src="//www.youtube.com/embed/W4DlMggBPvc" frameborder="0" allowfullscreen></iframe>
+</div>
+
+</br>
+
+<?php
+
+include ('../footer.php'); 
+
+?>
